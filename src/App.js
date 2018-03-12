153a80ede02c2531649c7633b95900bec114b09c
Will Rooney <william.rooney@ucdenver.edu>
2018-03-12 12:32:55 -0600

    Removed uneeded packages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css'
 import InstanceList from './InstanceList';
-//import ReactInterval from 'react-interval' // https://www.npmjs.com/package/react-interval
 
 const App = () => {
   return (
