4b87e40e9cf5ab8afb752b9c81080eb9d95f121c
Will Rooney <william.rooney@ucdenver.edu>
2018-03-11 19:15:55 -0600

    Interval Package Added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css'
 import InstanceList from './InstanceList';
+//import ReactInterval from 'react-interval' // https://www.npmjs.com/package/react-interval
 
 const App = () => {
   return (
